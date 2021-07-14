53e80565c857c117378dabb3cddb67cde95a02c2
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-14 23:34:16 +0300

    Task6 interface to print numbers

---
+++ tasks/src/senla/mikhalchenko/task6/service/PrintNumbersService.java
@@ -0,0 +1,24 @@
+package senla.mikhalchenko.task6.service;
+
+public interface PrintNumbersService {
+
+   void printNumberOne();
+
+   void printNumberTwo();
+
+   void printNumberThree();
+
+   void printNumberFour();
+
+   void printNumberFive();
+
+   void printNumberSix();
+
+   void printNumberSeven();
+
+   void printNumberEight();
+
+   void printNumberNine();
+
+   void printNumberZero();
+}
