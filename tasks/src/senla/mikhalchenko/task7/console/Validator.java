a0a9cd3210cc5156198fad8059adff9ec0158086
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-14 23:49:55 +0300

    Task7 Validator

---
+++ tasks/src/senla/mikhalchenko/task7/console/Validator.java
@@ -0,0 +1,17 @@
+package senla.mikhalchenko.task7.console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+    private Validator() {
+    }
+    private static Matcher matcher;
+    public static boolean isNumber(String answer) {
+        matcher = NUMBER_PATTERN.matcher(answer);
+        return matcher.matches();
+
+    }
+}
