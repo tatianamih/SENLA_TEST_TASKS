b8f97c9ea38812a00929b48249bf47028c1746bf
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-14 23:13:15 +0300

    Runner

---
+++ tasks/src/senla/mikhalchenko/task5/app/App.java
@@ -0,0 +1,15 @@
+package senla.mikhalchenko.task5.app;
+
+import senla.mikhalchenko.task5.console.Menu;
+/*Создать программу, которая будет:
+подсчитывать количество гласных в словах
+выводить слова отсортированные по кол-ву гласных (сперва те у которых больше гласных)
+делать первую гласную букву в слове заглавной
+Предложение вводится вручную. Разделитель пробел (“ ”).*/
+
+public class App {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mainMenu();
+    }
+}
