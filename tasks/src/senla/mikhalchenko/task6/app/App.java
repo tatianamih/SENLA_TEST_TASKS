168cb7e681a116c8680965be4f716d23a75816ef
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-14 23:24:50 +0300

    Task6 App

--- tasks/src/senla/mikhalchenko/task6/app/App.java
+++ tasks/src/senla/mikhalchenko/task6/app/App.java
@@ -6,6 +6,7 @@ import senla.mikhalchenko.task6.console.Menu;
 придумать свою, главное чтобы она читалась на экране.(*) Дополнительно: сделайте так, чтобы «большая цифра» состояла
  не из ‘*’, а из соответствующих маленьких(обычных) цифр.
  */
+
 public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
