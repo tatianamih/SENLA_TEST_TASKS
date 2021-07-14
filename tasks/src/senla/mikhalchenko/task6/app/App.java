abfd3f8926eafd2db5773379d94ac72f6416b100
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-14 23:23:57 +0300

    Task5 Service

---
+++ tasks/src/senla/mikhalchenko/task6/app/App.java
@@ -0,0 +1,14 @@
+package senla.mikhalchenko.task6.app;
+
+import senla.mikhalchenko.task6.console.Menu;
+
+/*Создайте программу, которая будет получать число и печатать его в следующем формате (формы цифр можно
+придумать свою, главное чтобы она читалась на экране.(*) Дополнительно: сделайте так, чтобы «большая цифра» состояла
+ не из ‘*’, а из соответствующих маленьких(обычных) цифр.
+ */
+public class App {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mainMenu();
+    }
+}
